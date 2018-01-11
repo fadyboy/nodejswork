fd44517c60d3321dd5ba9b75d632b81f03bf49be
Anthony Fadero <anthonyf@ecobee.com>
2018-01-11 18:30:13 -0500

    Added promises.js file

--- playground/promises2.js
+++ playground/promises2.js
@@ -1,11 +1,31 @@
 const request = require('request');
 
 var geocodeAddress = (address) => {
-    return new Promise((resolve, reject)=>{
-        resolve()
-    })
-    request({
-        url: `https://maps.googleapis.com/maps/api/geocode/json?address=${address}`,
-        json: true
-    })
+   return new Promise((resolve, reject) => {
+       debugger;
+       request({
+           url: `https://maps.googleapis.com/maps/api/geocode/json?address=${address}`,
+           json: true
+       }, (error, response, body) => {
+           if(error){
+               reject("Error connecting to server");
+           } else if(body.status === "ZERO_RESULTS"){
+               reject("No results for address");
+           } else if(body.status === "OK"){
+               resolve({
+                address: body.results[0].formatted_address,
+                lattitude: body.results[0].geometry.location.lat,
+                longitude: body.results[0].geometry.location.lng
+               });
+           }
+       });
+   })
 }
+
+geocodeAddress("8 Agolu avenue")
+ .then((res) =>{
+     console.log(JSON.stringify(res, undefined, 2));
+ })
+ .catch((errorMsg)=>{
+     console.log(errorMsg);
+ })
